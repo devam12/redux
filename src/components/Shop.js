c1fe8164697153878059c3eb5ba6b1c092f0f043
Devam <devampanchasara12@gmail.com>
2023-03-25 12:16:03 +0530

    Redux Project Done

---
+++ src/components/Shop.js
@@ -0,0 +1,17 @@
+import { React } from 'react'
+
+
+
+const Notes = (props) => {
+
+
+    return (
+        <div className="container">
+            <h2> Deposit & Withraw money</h2>
+            <button type="button" className="btn btn-primary mx-2">+</button>
+            <button type="button" className="btn btn-primary mx-2">-</button>
+        </div>
+    )
+}
+
+export default Notes
---
+++ src/state/action-creator/index.js
@@ -0,0 +1,18 @@
+export const deposite = (amount) =>{
+    return (dispath) =>{
+        dispath({
+            type : "diposite",
+            payload : amount,
+        })
+    }
+}
+
+export const withdraw = (amount) =>{
+    return (dispath) =>{
+        dispath({
+            type : "withdraw",
+            payload : amount,
+        })
+    }
+}
+
